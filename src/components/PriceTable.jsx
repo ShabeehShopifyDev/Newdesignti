e603c8f1c5e383f593920b4389e58a8d88f14749
Shabeeh UL Abbas <shabeeh.techintegrations@gmail.com>
2025-04-10 20:41:32 +0500

    Update PriceTable.jsx

--- src/components/PriceTable.jsx
+++ src/components/PriceTable.jsx
@@ -1,6 +1,6 @@
 import { CircleCheck } from 'lucide-react';
 import React from 'react';
-import Button from './Button';
+import Button from './button';
 
 function PricingTable({ tables, heading }) {
 
